9a0bf36ff37faa8c80c0fe1e1bbdc132bd7f881b
Debora Jaimes <deboranjaimes@gmail.com>
2021-10-23 18:23:46 -0400

    Commit Inicial

---
+++ src/components/AppBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    class="
+      px-6
+      py-2
+      transition
+      ease-in
+      duration-200
+      uppercase
+      rounded-full
+      hover:bg-gray-800 hover:text-white
+      border-2 border-gray-900
+      focus:outline-none
+    "
+  >
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
